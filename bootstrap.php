00845e7deaac4825e36ced7ec9dfddc8a32c916d
lauro <lauropimentel12@gmail.com>
2023-12-09 13:20:48 -0300

    src para app bootsrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ xdebug_set_filter(
     XDEBUG_FILTER_CODE_COVERAGE,
     XDEBUG_PATH_INCLUDE,
     [
-        __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR, // Include your source directory
+        __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR, // Include your source directory
         __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR, // Include your test directory
         // Add other directories or files you want to include for code coverage
     ]
