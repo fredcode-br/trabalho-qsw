ee3223ec95d52d683781e00bcc1c1c931867ce83
lauro <lauropimentel12@gmail.com>
2023-12-09 10:58:24 -0300

    testes

--- tests/InscricaoTest.php
+++ tests/InscricaoTest.php
@@ -26,11 +26,8 @@ class InscricaoTest extends TestCase
         // Simula um usuário e uma turma para inscrição
         $userId = 1; // ID do usuário
         $classId = 1; // ID da turma
-
         $this->abrirTurma($classId);
-        
         $enrollment = new Enrollment(null, $userId, $classId);
-
         // Testa se a inscrição é bem-sucedida
         $enrollmentId = $enrollment->enroll($userId, $classId);
         // Verifica se o ID retornado é um número válido (ou seja, a inscrição foi bem-sucedida)
@@ -77,15 +74,7 @@ class InscricaoTest extends TestCase
         // erro eh para dar confilto e nao ta dando
         $userId = 1; // ID do usuário
         $classId = 2; // ID da turma
-       
-        $mockConnection = $this->getMockBuilder(Connection::class)
-        ->disableOriginalConstructor()
-        ->getMock();
 
-        // Replace the actual Connection::getConn() method with the mock
-        $this->getMockBuilder(Section::class)->getMock()
-        ->method('updateSectionInDatabase');
-        
         $this->abrirTurma($classId);
         $enrollment = new Enrollment(null, $userId, $classId);
         $enrollment->enroll($userId, $classId);
@@ -94,8 +83,45 @@ class InscricaoTest extends TestCase
         $idDaNovaTurmaComHorarioConflitante = 5;
         $confilto = Section::checkScheduleConflict($userId, $idDaNovaTurmaComHorarioConflitante, null);
         // Testa se é retornado um conflito
-        $this->assertFalse($confilto=='');
+        $this->assertTrue($confilto=='');
     }
+
+    public function testInscricaoSemTurma(){
+        // Simula uma inscricao mal sucedida devido a Inscricao Sem Turma
+        $turmaInexistente = 0;
+        $userId = 1; // ID do usuário
+        $enrollment = new Enrollment(null, $userId, $turmaInexistente);
+        $this->expectException(Exception::class);
+        $enrollment->enroll($userId, $turmaInexistente);
+    }
+
+    public function testInscricaoSemUsuario(){
+        // Simula uma inscricao mal sucedida devido a Inscricao Sem Usuario
+        $turmaInexistente = 1;
+        $userId = 0; // ID do usuário
+        // cria uma inscricao
+        $enrollment = new Enrollment(null, $userId, $turmaInexistente);
+        $this->expectException(Exception::class);
+        $enrollment->enroll($userId, $turmaInexistente);
+    }
+
+    public function testRemoverInscricao(){
+        // Simula uma remocao de inscricao bem sucedida
+        $inscricaoId = 1; // ID do usuário 
+        $enrollment=Enrollment::unenroll($inscricaoId);
+        $this->assertTrue($enrollment);
+    }
+    public function testRemoverInscricaoMalSucedida(){
+        // Simula uma remocao de inscricao mal sucedida
+        $inscricaoId = 0; // ID do usuário 
+        $result = Enrollment::unenroll($inscricaoId);
+        $this->assertFalse($result);
+    }
+
+
+
+    
+
 
 
 }
